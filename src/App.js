4971e139a299ed19549fe0f8ca0100295781bd94
Gladyreigh Dela Cruz <tgladyreigh@gmail.com>
2024-10-31 05:25:00 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 import WhatsAppContact from './components/WhatsAppContact';
 import Notification from './components/Notification';
 import RepairForm from './components/Repair/RepairForm';
-import AboutUs from './components/AboutUs/AboutUs'; // 
+import AboutUs from './components/AboutUs/AboutUs'; 
 import ContactUs from './components/Contact/ContactUs'; 
 
 function App() {
@@ -84,6 +84,7 @@ function App() {
             <Route path="/repair" element={<RepairForm />} />
             <Route path="/about" element={<AboutUs />} /> 
             <Route path="/contact" element={<ContactUs />} />
+            <Route path="*" element={<Navigate to="/" />} /> {/* Redirect to home on 404 */}
           </Routes>
           <WhatsAppContact />
         </main>
